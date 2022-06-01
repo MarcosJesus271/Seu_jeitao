9999f7fab2a9c71fef2f2aa8b1cabe52125f386d
Marcos Jesus <viniciuspop1@hotmail.com>
2022-06-01 16:06:36 -0300

    Template modified

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes_app/home.html')
+    return render(request, 'recipes_app/home.html', context={'name': 'Marcos Jesus',})
 
 def contato(request):
-    return HttpResponse("Hello Contato!")
+    return render(request, 'recipes_app/contato.html')
 
 def sobre(request):
     return HttpResponse("Hello Sobre")
