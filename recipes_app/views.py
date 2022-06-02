06f0cf1616cff4a23161ff77e482c932c6b8f1fb
Marcos Jesus <viniciuspop1@hotmail.com>
2022-06-01 23:53:08 -0300

    Create partials for code html

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes_app/home.html', context={'name': 'Marcos Jesus',})
+    return render(request, 'recipes_app/pages/home.html', context={'name': 'Marcos Jesus',})
 
