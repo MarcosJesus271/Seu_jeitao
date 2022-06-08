4b97185d0251d12a1e1a9a73b43fbc25ec202932
Marcos Jesus <viniciuspop1@hotmail.com>
2022-06-08 14:40:42 -0300

    Add fake users in cards for home.htm

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes_app/pages/home.html', context={'name': 'Marcos Jesus',})
+    return render(request, 'recipes_app/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+        })
 
 def recipe(request, id):
-    return render(request, 'recipes_app/pages/recipe-view.html', context={'name': 'Marcos Jesus',})
+    return render(request, 'recipes_app/pages/recipe-view.html', context={
+        'recipe': make_recipe(),})
 
