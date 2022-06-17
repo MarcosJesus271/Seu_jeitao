32b69b7d60a510cf2e884da47f26e384b1d31047
Marcos Jesus <viniciuspop1@hotmail.com>
2022-06-17 11:28:07 -0300

    no recipes in case not recipes in db home

--- recipes/views.py
+++ recipes/views.py
@@ -14,13 +14,7 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            category__id=category_id,
-            is_published=True,
-        ).order_by('-id')
-    )
-
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id,is_published=True,).order_by('-id'))
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | '
