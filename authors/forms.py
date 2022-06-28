b6a0d03845fcc5513565b4ee0ba4b57221d041c4
Marcos Jesus <viniciuspop1@hotmail.com>
2022-06-28 13:23:22 -0300

    placeholder in forms

--- authors/forms.py
+++ authors/forms.py
@@ -5,7 +5,35 @@ from django.contrib.auth.models import User
 class RegisterFrom(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name',
-                  'username', 'email',
-                  'password',
-                  ]
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid.'
+        }
+        erros_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here',
+                'class': 'input text-input'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here.'
+            })
+        }
