3f59870c5b96107adb848c3f19ddab5ea482f54c
Marcos Jesus <viniciuspop1@hotmail.com>
2022-06-29 13:51:39 -0300

    init dashboard

--- authors/views.py
+++ authors/views.py
@@ -33,6 +33,7 @@ def register_create(request):
 
         del(request.session['register_form_data'])
         return redirect(reverse('authors:login'))
+
     return redirect('authors:register')
 
 
@@ -49,7 +50,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -65,10 +65,24 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
+    if not request.POST:
+        messages.error(request, 'Invalid logout request')
+        return redirect(reverse('authors:login'))
+
+    if request.POST.get('username') != request.user.username:
+        messages.error(request, 'Invalid logout user')
+        return redirect(reverse('authors:login'))
+
+    messages.success(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
