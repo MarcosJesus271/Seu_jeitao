7ab17af7ad1506b1662cc7f91aad2e7084d7dfae
Marcos Jesus <viniciuspop1@hotmail.com>
2022-07-05 15:46:35 -0300

    method class view

--- authors/urls.py
+++ authors/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
          name='dashboard_recipe_new'),
     path('dashboard/recipe/delete/', views.dashboard_recipe_delete,
          name='dashboard_recipe_delete'),
-    path('dashboard/recipe/<int:id>/edit/', views.dashboard_recipe_edit,
+    path('dashboard/recipe/<int:id>/edit/', views.DashboardRecipe.as_view(),
          name='dashboard_recipe_edit'),
 ]
