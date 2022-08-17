a987f10b865b2e8d38149ab87a36e8d9e0e81859
Marcos Jesus <viniciuspop1@hotmail.com>
2022-08-17 15:19:55 -0300

    Force Update

--- authors/forms/login.py
+++ authors/forms/login.py
@@ -5,8 +5,8 @@ from utils.django_forms import add_placeholder
 class LoginForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        add_placeholder(self.fields['username'], 'Type your username')
-        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['username'], 'Escreva o nome de usúario')
+        add_placeholder(self.fields['password'], 'Escreva o a senha do usúario')
 
     username = forms.CharField()
     password = forms.CharField(
